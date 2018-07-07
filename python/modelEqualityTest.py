a6be5362360caf74ab73a27ee9ab481de4299947
polinankoleva <polina.n.koleva@gmail.com>
2018-07-07 16:42:42 +0200

    Change model equality test with reading stored peers.

--- python/modelEqualityTest.py
+++ python/modelEqualityTest.py
@@ -26,23 +26,6 @@ class EqualityTest():
             # citeulike_paper_id | user_id | paper_id|
             fold.training_data_frame = fold.training_data_frame.drop("timestamp", "citeulike_user_hash")
 
-
-            # removes from training data frame those users which do not appear in the test set, no need
-            # a model for them to be trained
-            test_user_ids = fold.test_data_frame.select(self.userId_col).distinct()
-            fold.training_data_frame = fold.training_data_frame.join(test_user_ids, self.userId_col)
-
-            # test only for 10 random users
-            user_ids = fold.training_data_frame.select("user_id").distinct().collect()
-            shuffle(user_ids)
-            user_ids = user_ids[:50]
-            Logger.log("Test for " + str(len(user_ids)) + " user/s")
-
-            filter_condition = "user_id ==" + str(user_ids[0][0])
-            for user_id in user_ids[1:]:
-                filter_condition += " or user_id ==" + str(user_id[0])
-            users_dataset = fold.training_data_frame.filter(filter_condition)
-
             # load peers so you can remove the randomization factor when comparing
             # 1) Peer papers sampling
             nps = PeerPapersSampler(fold.papers_corpus, self.peer_papers_count, paperId_col=self.paperId_col,
@@ -50,8 +33,24 @@ class EqualityTest():
                                     output_col="peer_paper_id")
 
             # schema -> user_id | citeulike_paper_id | paper_id | peer_paper_id |
-            peers_dataset = nps.transform(users_dataset)
+            peers_dataset = nps.load_peers(self.spark, i)
 
+            # removes from training data frame those users which do not appear in the test set, no need
+            # a model for them to be trained
+            test_user_ids = fold.test_data_frame.select(self.userId_col).distinct()
+            fold.training_data_frame = fold.training_data_frame.join(test_user_ids, self.userId_col)
+            peers_dataset = peers_dataset.join(test_user_ids, self.userId_col)
+
+            # test only for 10 random users
+            user_ids = peers_dataset.select("user_id").distinct().collect()
+            shuffle(user_ids)
+            user_ids = user_ids[:50]
+            Logger.log("Test for " + str(len(user_ids))+ " user/s")
+
+            filter_condition = "user_id ==" + str(user_ids[0][0])
+            for user_id in user_ids[1:]:
+                filter_condition += " or user_id ==" + str(user_id[0])
+            test_peer_paper = peers_dataset.filter(filter_condition)
 
             # peer_paper_lda_vector, paper_lda_vector
             papersPairBuilder = PapersPairBuilder(self.pairs_generation, "ims",
@@ -62,7 +61,7 @@ class EqualityTest():
                                                       label_col="label")
             # 2) pair building
             # paper_id | peer_paper_id | user_id | citeulike_paper_id | lda_vector | peer_paper_lda_vector | features | label
-            dataset = papersPairBuilder.transform(peers_dataset)
+            dataset = papersPairBuilder.transform(test_peer_paper)
             dataset = dataset.drop("peer_paper_lda_vector", "lda_vector")
             dataset.persist()
 
@@ -90,6 +89,7 @@ class EqualityTest():
             Logger.log("Unpersist the data")
             dataset.unpersist()
             Logger.log("Equality test successfully completed for fold:" + str(i))
+
         end_time = datetime.datetime.now() - start_time
         Logger.log("Test finished in :" + str(end_time))
         Logger.log("Equality test completed.")
